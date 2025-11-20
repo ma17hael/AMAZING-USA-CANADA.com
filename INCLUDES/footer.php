9137c2b5f5bc9a6b21731171243c7e37f0865d78
Malthael <ma.1thea1.wright@gmail.com>
2025-11-20 08:59:14 +0100

    Mise à jour du choix de langue du footer

--- INCLUDES/footer.php
+++ INCLUDES/footer.php
@@ -3,27 +3,27 @@
 
         <!-- Colonne : Informations -->
         <div class="footer-column">
-            <h4>Documents Obligatoires</h4>
+            <h4><?=$translations['footer-mandatory']?></h4>
             <ul>
-                <li><a href="mentions-légales.php">Mentions Légales</a></li>
-                <li><a href="mentions-légales.php">Condition Générale d'Utilisation</a></li>
-                <li><a href="mentions-légales.php">Condition Générale de Vente</a></li>
+                <li><a href="mentions-légales.php"><?=$translations['footer-legalnotice']?></a></li>
+                <li><a href="mentions-légales.php"><?=$translations['footer-CGU']?></a></li>
+                <li><a href="mentions-légales.php"><?=$translations['footer-CGV']?></a></li>
             </ul>
         </div>
 
         <!-- Colonne : Navigation -->
         <div class="footer-column">
-            <h4>Navigation</h4>
+            <h4>Navigation :</h4>
             <ul>
-                <li><a href="index.php">Accueil</a></li>
-                <li><a href="cartes.php">Nos cartes</a></li>
-                <li><a href="contact.php">Contact</a></li>
+                <li><a href="index.php"><?=$translations['header-home']?></a></li>
+                <li><a href="cartes.php"><?=$translations['header-maplist']?></a></li>
+                <li><a href="contact.php"><?=$translations['header-about']?></a></li>
             </ul>
         </div>
 
         <!-- Colonne : Réseaux -->
         <div class="footer-column">
-            <h4>Suivez-nous</h4>
+            <h4><?=$translations['footer-followUs']?></h4>
             <ul class="social-links">
                 <li><a href="#" aria-label="Facebook">Facebook</a></li>
             </ul>
