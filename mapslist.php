9c839fcb9942a67eb40efbf18be9f23155fbcf59
Malthael <ma.1thea1.wright@gmail.com>
2025-12-15 14:27:48 +0100

    Ajout des filtres accurate pour les cartes

--- mapslist.php
+++ mapslist.php
@@ -3,12 +3,31 @@
 include_once("INCLUDES/init.php");
 require_once 'INCLUDES/config.php';
 
-$sql = "SELECT S.ID_Map, M.Libelle_TypeFR, M.Libelle_TypeEN, S.StateMap, S.Map_NameFR, S.Map_NameEN, L.LibelleLocalisationFR, L.LibelleLocalisationEN, S.Prix
+$sqlMap = "SELECT S.ID_Map, S.Map_Type, M.Libelle_TypeFR, M.Libelle_TypeEN, S.StateMap, S.Map_NameFR, S.Map_NameEN, S.Approx_Localisation, L.LibelleLocalisationFR, L.LibelleLocalisationEN, S.Prix
         FROM Statesmap S
         INNER JOIN MapTypes M ON M.Id_TypeMap = S.Map_Type
         INNER JOIN Localisation L ON L.ID_Localisation = S.Approx_Localisation;";
-$stmtUS = $pdo->prepare($sql);
-$stmtUS->execute();
+$stmtMap = $pdo->prepare($sqlMap);
+$stmtMap->execute();
+
+$sqlLoc = "SELECT L.ID_Localisation, L.LibelleLocalisationFR, L.LibelleLocalisationEN
+        FROM Localisation L;";
+$stmtLoc = $pdo->prepare($sqlLoc);
+$stmtLoc->execute();
+$localisations = $stmtLoc->fetchAll(PDO::FETCH_ASSOC);
+
+$sqlType = "SELECT T.ID_TypeMap, T.Libelle_TypeFR, T.Libelle_TypeEN
+            FROM MapTypes T;";
+$stmtType = $pdo->prepare($sqlType);
+$stmtType->execute();
+$types = $stmtType->fetchAll(PDO::FETCH_ASSOC);
+
+$sqlPrice = "SELECT MAX(S.Prix) AS 'PrixMax'
+             FROM Statesmap S;";
+$stmtPrice = $pdo->prepare($sqlPrice);
+$stmtPrice->execute();
+$maxPrice = $stmtPrice->fetch(PDO::FETCH_ASSOC);
+$prixMax = (float)$maxPrice['PrixMax'];
 
 ?>
 <!DOCTYPE html>
@@ -35,11 +54,11 @@ $stmtUS->execute();
         </div>
 
         <!-- Zone de filtre -->
-        <div class="filters"">
-        <select id=" filter-type">
+        <div class="filters">
+        <select id="filter-type">
             <option value="">Tous types</option>
             <?php foreach ($types as $type): ?>
-                <option value="<?= htmlspecialchars($type['Id_TypeMap']) ?>"><?= htmlspecialchars($type["Libelle_Type$langBDD"]) ?></option>
+                <option value="<?= htmlspecialchars($type['ID_TypeMap']) ?>"><?= htmlspecialchars($type["Libelle_Type$langBDD"]) ?></option>
             <?php endforeach; ?>
             </select>
 
@@ -52,23 +71,26 @@ $stmtUS->execute();
 
             <div class="price-filter">
                 <label for="price-range">Prix : </label>
-                <input type="range" id="price-min" min="0" max="500" value="0" step="1">
-                <input type="range" id="price-max" min="0" max="500" value="500" step="1">
-                <span id="price-display">0€ - 500€</span>
+                <input type="range" id="price-min" min="0" max="<?=$prixMax?>" value="0" step="0.01">
+                <input type="range" id="price-max" min="0" max="<?=$prixMax?>" value="<?=$prixMax?>" step="0.01">
+                <span id="price-display" data-currency="<?= htmlspecialchars($translations['filter-money']) ?>">0<?=$translations['filter-money']?> - <?=$prixMax?><?=$translations['filter-money']?><?=$translations['filter-money']?></span>
             </div>
         </div>
         <div class="card-container">
-            <?php while ($map = $stmtUS->fetch(PDO::FETCH_ASSOC)): ?>
+            <?php while ($map = $stmtMap->fetch(PDO::FETCH_ASSOC)): ?>
                 <?php
                 //Conversion de Blob à base64
                 $imageBase64 = base64_encode($map['StateMap']);
                 $imageSrc = 'data:image/jpeg;base64,' . $imageBase64;
                 ?>
-                <div class="mapcard">
+                <div class="mapcard"
+                    data-type="<?=(int)$map['Map_Type']?>"
+                    data-location="<?=(int)$map['Approx_Localisation']?>"
+                    data-price="<?=(float)$map['Prix']?>">
                     <img src="<?= $imageSrc ?>" alt=<?= htmlspecialchars($map["Map_Name$langBDD"]) ?>>
                     <h3><?= htmlspecialchars($map["Map_Name$langBDD"]) ?></h3>
                     <p><strong><?= $translations['home-mapshowcase-card-type'] ?></strong><?= htmlspecialchars($map["Libelle_Type$langBDD"]) ?></p>
-                    <p><strong><?= $translations['home-mapshowcase-card-localisation'] ?></strong><?= htmlspecialchars($map["LibelleLocalisation$langBDD"]) ?>
+                    <p><strong><?= $translations['home-mapshowcase-card-localisation'] ?></strong><?= htmlspecialchars($map["LibelleLocalisation$langBDD"]) ?></p>
                     <p><strong><?= $translations['home-mapshowcase-card-price'] ?></strong><?= htmlspecialchars($map['Prix']) ?><?= $translations['home-mapshowcase-card-money'] ?></p>
                     <div class="mapcard-actions">
                         <form action="cart.php" method="POST">
@@ -89,8 +111,7 @@ $stmtUS->execute();
     </main>
     <?php include_once('INCLUDES/footer.php'); ?>
 </body>
-<script src="JAVASCRIPT/price-display.js"></script>
-<script src="JAVASCRIPT/imageModal.js"></script>
-<script src="JAVASCRIPT/map-switch.js"></script>
-
+    <script src="JAVASCRIPT/map-filtering.js"></script>
+    <script src="JAVASCRIPT/price-display.js"></script>
+    <script src="JAVASCRIPT/imageModal.js"></script>
 </html>
