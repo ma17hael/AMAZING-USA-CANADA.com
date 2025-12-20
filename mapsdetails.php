3a94e8f33589cdc8c2214e092c85beb8ea12cc3a
Malthael <ma.1thea1.wright@gmail.com>
2025-12-20 14:31:11 +0100

    Ajout des pages de details des cartes (Améliorations Possible)

---
+++ mapsdetails.php
@@ -0,0 +1,115 @@
+<?php
+include_once("INCLUDES/init.php");
+require_once 'INCLUDES/config.php';
+
+if (!isset($_GET['id']) || !ctype_digit($_GET['id'])) {
+    http_response_code(404);
+    exit('Carte Introuvable');
+}
+
+$id = (int) $_GET['id'];
+
+$stmt = $pdo->prepare('SELECT S.*, L.LibelleLocalisationFR, L.LibelleLocalisationEN, M.Libelle_TypeFR, M.Libelle_TypeEN
+                       FROM StatesMap S
+                       INNER JOIN MapTypes M ON M.Id_TypeMap = S.Map_Type
+                       INNER JOIN Localisation L ON L.ID_Localisation = S.Approx_Localisation
+                       WHERE ID_Map = :id');
+$stmt->execute(['id' => $id]);
+$map = $stmt->fetch(PDO::FETCH_ASSOC);
+
+$stmt = $pdo->prepare('SELECT Flag
+                       FROM StateFlag
+                       WHERE Id_Map = :id');
+$stmt->execute(['id' => $id]);
+$flags = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+if (!$map) {
+    http_response_code(404);
+    exit('Carte inexistante');
+}
+?>
+<!DOCTYPE html>
+<html lang="<?= $lang ?>">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= htmlspecialchars($map["Map_Name$langBDD"]) ?> - AMAZING-USA-CANADA.com</title>
+    <link rel="stylesheet" href="CSS/mapsdetails.css">
+    <link rel="stylesheet" href="CSS/header.css">
+    <link rel="stylesheet" href="CSS/footer.css">
+    <link rel="icon" href="INCLUDES/ICONS/favicon.ico" type="image/x-icon">
+</head>
+
+<body>
+    <?php include_once("INCLUDES/header.php"); ?>
+    <main>
+        <div class="details-main-title">
+            <h1>Données Essentiels de la Carte</h1>
+        </div>
+        <section class="maps-essentials">
+            <div class="map-static">
+                <?php
+                //Conversion de Blob à base64
+                $imageBase64 = base64_encode($map['StateMap']);
+                $imageSrc = 'data:image/jpeg;base64,' . $imageBase64;
+                ?>
+                <img src=<?= $imageSrc ?> alt=<?= htmlspecialchars($map["Map_Name$langBDD"]) ?> data-modal-image>
+            </div>
+            <div class="map-details">
+                <h2><?= htmlspecialchars($map["Map_Name$langBDD"]) ?></h2>
+                <div class="map-flags">
+                    <?php foreach ($flags as $flag): ?>
+                        <?php
+                        $flagBase64 = base64_encode($flag['Flag']);
+                        $flagSrc = 'data:image/png;base64,' . $flagBase64;
+                        ?>
+                        <img src="<?= $flagSrc ?>" alt="Drapeau" class="flag-img">
+                    <?php endforeach; ?>
+                </div>
+                <p>
+                    <strong>Localisation :</strong>
+                    <?= htmlspecialchars($map["LibelleLocalisation$langBDD"]) ?>
+                </p>
+                <p>
+                    <strong>Type de carte :</strong>
+                    <?= htmlspecialchars($map["Libelle_Type$langBDD"]) ?>
+                </p>
+                <p>
+                    <strong>Prix :</strong>
+                    <?= htmlspecialchars($map['Prix']) ?> €
+                </p>
+                <a href="#" class="btn-map">
+                    Ajouter au panier
+                </a>
+            </div>
+        </section>
+        <div class="details-main-title">
+            <h1>Informations complémentaires</h1>
+        </div>
+        <section class="maps-essentials">
+            <div class="countrymap-static">
+                <?php
+                //Conversion de Blob à base64
+                $imageBase64 = base64_encode($map['Country_Map']);
+                $imageSrc = 'data:image/jpeg;base64,' . $imageBase64;
+                ?>
+                <img src=<?= $imageSrc ?> alt=<?= htmlspecialchars($map["Map_Name$langBDD"]) ?> data-modal-image>
+            </div>
+            <div class="map-details">
+                <h2>Son Emplacement sur la carte du Pays :</h2>
+                <p>Cette carte vous permet donc de vous situer sur la carte du pays et de le retrouver plus facilement.
+                    Cela vous permettra ainsi de plus facilement faire vos trajets sans passer par les pack de cartes.
+                </p>
+            </div>
+        </section>
+        <div id="image-modal" class="modal">
+            <span class="close">&times;</span>
+            <img class="modal-content" id="modal-img">
+        </div>
+    </main>
+    <?php include_once('INCLUDES/footer.php'); ?>
+</body>
+<script src="JAVASCRIPT/imageModal.js"></script>
+
+</html>
