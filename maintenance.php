7756f52a3b96e7be048f0ea76a7286db98b160b7
Malthael <ma.1thea1.wright@gmail.com>
2026-04-07 21:05:13 +0200

    Départ du projet de page de maintenance

---
+++ maintenance.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Maintenance</title>
+    <!-- CSS externe -->
+    <link rel="stylesheet" href="/assets/css/maintenance.css">
+    <!-- Google Fonts (optionnel) -->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+</head>
+<body>
+
+<div class="container">
+    <img src="/MAINTENANCE/ASSETS/CANADALogo.webp" class="logo" alt="logo">
+
+    <h1>🚧 Site en maintenance</h1>
+    <p>On améliore l'expérience, reviens vite 😉</p>
+</div>
+
+</body>
+</html>
