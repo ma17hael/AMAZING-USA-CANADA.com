fcdcb90c00e860a406fc84dcf77f147c62e7132b
Malthael <ma.1thea1.wright@gmail.com>
2026-05-06 09:16:38 +0200

    Ajout d'une bannière comme sur l'ancien site

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
 require_once __DIR__ . '/../GLOBAL-INCLUDES/CONFIGS/bootstrap.php';
-
-var_dump(function_exists('apcu_fetch'));
-?>
 ?>
 <!DOCTYPE html>
 <html lang="<?= $lang ?>">
@@ -18,5 +15,15 @@ var_dump(function_exists('apcu_fetch'));
 </head>
 <body>
     <?php include_once('LAYOUT/header.php');?>
+    <!-- BANNIERE -->
+    <section class="hero-banner">
+        <div class="hero-content">
+            <h1>Bienvenue sur Amazing-USA-Canada.com</h1>
+            <p>Concevez vous-même votre Road Trip, avec la plus grande base de données
+                de sites à visiter, présente sur le NET !
+            </p>
+            <a href="#" class="btn-hero">Liste de nos cartes</a>
+        </div>
+    </section>
 </body>
 <script src="http://assets.amazing-usa-canada.local/JS/global.js"></script>
