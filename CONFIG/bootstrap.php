d365a041076abe29761bf6cbbd53f789e8354210
ma17hael <ma.1thea1.wright@gmail.com>
2026-06-01 23:51:56 +0200

    Ajout d'un point d'entrée de la plupart des fichiers applicatif

---
+++ CONFIG/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+require_once __DIR__ . "/../../vendor/autoload.php";
+require_once __DIR__ . '/config.php';
+
+session_start();
+
+$settings = new App\Services\SettingService();
+$settings->preloadAll();
+
+$maintenance = new App\Services\MaintenanceService($settings);
+$maintenance->check();
