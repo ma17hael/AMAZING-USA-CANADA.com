7bcb1084697db599083ec06ad66d55b0857c4a58
ma17hael <ma.1thea1.wright@gmail.com>
2026-06-03 00:36:53 +0200

    Ajout d'un premier controlleur (Page d'accueil)

---
+++ APP/CONTROLLERS/HomeController.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Controllers;
+
+use App\Services\TranslationService;
+
+class HomeController {
+    public function index(array $params, $translator, $router): void {
+        require_once __DIR__ . '/../../VIEWS/HOME/index.php';
+    }
+}
