5f8cd0285dc86268fbf27de6db06e7cf1dab3f79
ma17hael <ma.1thea1.wright@gmail.com>
2026-06-04 01:12:01 +0200

    Ajout de l'authentificateur dans le controlleur et le routeur

--- APP/CONTROLLERS/HomeController.php
+++ APP/CONTROLLERS/HomeController.php
@@ -4,7 +4,7 @@ namespace App\Controllers;
 use App\Services\TranslationService;
 
 class HomeController {
-    public function index(array $params, $translator, $router): void {
+    public function index(array $params, $translator, $router, $auth): void {
         require_once __DIR__ . '/../../VIEWS/HOME/index.php';
     }
 }
