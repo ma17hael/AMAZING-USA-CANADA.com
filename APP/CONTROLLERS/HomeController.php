e4eb35c70be83cdea45564afd4fef3444345e3fa
ma17hael <ma.1thea1.wright@gmail.com>
2026-06-04 09:56:00 +0200

    Ajout de la sécurité des requetes dans le premier controlleur

--- APP/CONTROLLERS/HomeController.php
+++ APP/CONTROLLERS/HomeController.php
@@ -4,7 +4,7 @@ namespace App\Controllers;
 use App\Services\TranslationService;
 
 class HomeController {
-    public function index(array $params, $translator, $router, $auth): void {
+    public function index(array $params, $translator, $router, $auth, $request): void {
         require_once __DIR__ . '/../../VIEWS/HOME/index.php';
     }
 }
