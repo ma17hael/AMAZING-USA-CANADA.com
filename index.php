3a0edee81e5173a2eb5b4d36652b8999a637e3fd
ma17hael <ma.1thea1.wright@gmail.com>
2026-06-05 15:31:00 +0200

    Ajout du point d'entrée pour le dashboard

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+define('APP_SITE', 'dashboard');
+define('APP_VIEWS_PATH', __DIR__ . '/VIEWS');
+define('APP_ROUTES', 'dashboard');
+
+require_once __DIR__ . '/../GLOBAL-INCLUDES/CONFIG/bootstrap.php';
