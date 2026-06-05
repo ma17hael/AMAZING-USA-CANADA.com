0e0539223a163150ba77152b1453a17cac114513
ma17hael <ma.1thea1.wright@gmail.com>
2026-06-05 15:23:54 +0200

    Ajout des URL des sous domaines en constantes

--- CONFIG/config.php
+++ CONFIG/config.php
@@ -11,6 +11,10 @@ define('DB_PASSWORD', 'root');
 
 define('APP_NAME', 'Amazing USA Canada');
 define('APP_URL', APP_ENV === 'production' ? 'https://amazing-usa-canada.com' : 'http://amazing-usa-canada.local:8080');
+define('APP_URL_ASSETS', APP_ENV === 'production' ? 'https://assets.amazing-usa-canada.com' : 'http://assets.amazing-usa-canada.local:8080');
+define('APP_URL_API', APP_ENV === 'production' ? 'https://api.amazing-usa-canada.com' : 'http://api.amazing-usa-canada.local:8080');
+define('APP_URL_DASHBOARD', APP_ENV === 'production' ? 'https://dashboard.amazing-usa-canada.com' : 'http://dashboard.amazing-usa-canada.local:8080');
+define('APP_URL_MAINTENANCE', APP_ENV === 'production' ? 'https://maintenance.amazing-usa-canada.com' : 'http://maintenance.amazing-usa-canada.local:8080');
 
 define('ROOT_PATH', dirname(__DIR__, 2));
 define('STORAGE_PATH', ROOT_PATH . '/STORAGE');
